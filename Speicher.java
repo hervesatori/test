a63a36e98c33b411fc234a2a32f9e6cb53e48c06
hervesatori <satori_herve@yahoo.fr>
2016-04-26 19:07:08 +0200

    Delete Speicher.java

--- Speicher.java
+++
@@ -1,9 +0,0 @@
-package ch.ffhs.ftoop.p1.producerconsumer;
-
-//TODO Entfernen Sie den abstract modifier und implementieren Sie die fehlenden Methoden!
-public abstract class Speicher implements SpeicherIf {
-
-	private int wert;
-	private boolean hatWert = false;
-
-}
